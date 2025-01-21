c9675cd0681ecc2cc7b55329f0e246c934b49468
theactualdev <olayinkacodes@gmail.com>
2025-01-21 14:38:26 +0100

    fix: fixes build issues with initialized Navbar in Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Barlow_Semi_Condensed } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const barlowSemiCondensed = Barlow_Semi_Condensed({
   subsets: ["latin"],
