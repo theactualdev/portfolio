590dcc9b658a073f4004bd7445493237a6f8df98
theactualdev <olayinkacodes@gmail.com>
2025-09-10 15:07:47 +0100

    fix: resource failure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites(){
+    return [
+      {
+        source: "/resume.pdf",
+        destination: "resume.pdf"
+      }
+    ]
+  },
+
   compiler: {
     removeConsole: true
   }
